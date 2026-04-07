a90027f29dcba748f06a91e20ea8b9f387dc17c2
oramzy <giftoramabo@gmail.com>
2026-04-07 15:58:13 +0100

    Initial Commit

--- Driver/src/ui/AppSwitch.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Switch } from 'react-native';
-
-import { colors } from './theme';
-
-export function AppSwitch(props: { value: boolean; onValueChange: (v: boolean) => void }) {
-  return <Switch value={props.value} onValueChange={props.onValueChange} trackColor={{ true: colors.goldSoft }} />;
-}
